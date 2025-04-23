551109736819b352b54dccfe0aea96db35fbf965
DrasaClone <drasaclone@gmail.com>
2025-04-23 22:06:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const pubnub = new PubNub({
     publishKey: 'pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4',
-    subscribeKey: 'YOUR_SUBSCRIBE_KEY'
+    subscribeKey: 'sub-c-0269ec54-430f-41b1-8a33-4200f566fcbb'
 });
 
 pubnub.subscribe({ channels: ['chat_channel'] });
