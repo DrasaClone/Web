f667049efaa841d28ac887d0fc5ea7131af3404e
DrasaClone <drasaclone@gmail.com>
2025-04-23 16:41:36 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,14 @@
 // assets/js/firebase-config.js
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCeYwTT7E8bi7bccIrc20MTe5S4r0e0wUI",
+  authDomain: "webai-7642b.firebaseapp.com",
+  databaseURL: "https://webai-7642b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webai-7642b",
+  storageBucket: "webai-7642b.firebasestorage.app",
+  messagingSenderId: "967881370128",
+  appId: "1:967881370128:web:e5c4b06e4f70f55a68b895",
+  measurementId: "G-61XJ390Q30"
 };
 
 // Khởi tạo Firebase
