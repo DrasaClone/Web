4b2517b49547155171ed3864f970fe81125a9950
DrasaClone <drasaclone@gmail.com>
2025-04-23 22:04:05 +0700

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://DATABASE_NAME.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID"
+  apiKey: "AIzaSyCeYwTT7E8bi7bccIrc20MTe5S4r0e0wUI",
+  authDomain: "webai-7642b.firebaseapp.com",
+  databaseURL: "https://webai-7642b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webai-7642b",
+  storageBucket: "webai-7642b.firebasestorage.app",
+  messagingSenderId: "967881370128",
+  appId: "1:967881370128:web:e5c4b06e4f70f55a68b895",
+  measurementId: "G-61XJ390Q30"
 };
 
 // Khởi tạo Firebase
