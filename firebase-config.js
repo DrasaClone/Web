6f1337580826ebf00e9fe5c14e35e347e351b622
DrasaClone <drasaclone@gmail.com>
2025-04-23 08:26:52 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,11 @@
-firebase.initializeApp({ /* YOUR_FIREBASE_CONFIG */ });
+firebase.initializeApp({ apiKey: "AIzaSyCeYwTT7E8bi7bccIrc20MTe5S4r0e0wUI",
+  authDomain: "webai-7642b.firebaseapp.com",
+  databaseURL: "https://webai-7642b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webai-7642b",
+  storageBucket: "webai-7642b.firebasestorage.app",
+  messagingSenderId: "967881370128",
+  appId: "1:967881370128:web:e5c4b06e4f70f55a68b895",
+  measurementId: "G-61XJ390Q30" });
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const storage = firebase.storage();
