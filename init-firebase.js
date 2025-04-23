9595b28b64b9a44e736939f30f02bb8baa6ae4f1
DrasaClone <drasaclone@gmail.com>
2025-04-23 21:59:24 +0700

    Create init-firebase.js

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://DATABASE_NAME.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "G-MEASUREMENT_ID"
+};
+
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
