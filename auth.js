dcc11ddc76ef5f9cf11ee216e80b180d1de96349
DrasaClone <drasaclone@gmail.com>
2025-04-23 08:52:41 +0700

    Delete auth.js

--- auth.js
+++
@@ -1,5 +0,0 @@
-import { auth, db } from './firebase-config.js';
-auth.onAuthStateChanged(async user => {
-  if(user) { /* load profile, pubnub.setUUID, show chat */ }
-  else { /* show login */ }
-});
