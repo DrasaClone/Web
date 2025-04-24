fcf8a1d39213c596d351e92e422170cd32b00126
DrasaClone <drasaclone@gmail.com>
2025-04-24 22:31:04 +0700

    Update pubnub-chat.js

--- pubnub-chat.js
+++ pubnub-chat.js
@@ -1,6 +1,6 @@
 // pubnub-chat.js
 export const pubnub = new PubNub({
-  publishKey: "YOUR_PUBNUB_PUBLISH_KEY",
+  publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4",
   subscribeKey: "YOUR_PUBNUB_SUBSCRIBE_KEY",
   uuid: `user-${Date.now()}`
 });
