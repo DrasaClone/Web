b353550cf9726f7ea5fd55c2601226a0e75aab25
DrasaClone <drasaclone@gmail.com>
2025-04-25 21:11:12 +0700

    Create firebase-setup.js

---
+++ firebase-setup.js
@@ -0,0 +1,13 @@
+/* firebase-setup.js */
+// Thay YOUR_... bằng thông tin cấu hình của bạn
+const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
