53e1944ca69725ecd7effa5ed459a11e1a1c1719
DrasaClone <drasaclone@gmail.com>
2025-04-27 14:02:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // main.js
-
 import { auth } from "./config.js";
 import { monitorAuthState, setupAuthListeners } from "./auth.js";
 import { setupPostCreation, loadPosts, setupPostSearch } from "./posts.js";
 import { setupCommentCreation } from "./comments.js";
 import { setupChat } from "./chat.js";
 import { setupNotifications } from "./notifications.js";
+// Bạn có thể import thêm hàm từ friends.js nếu muốn thiết lập giao diện kết bạn
+import { listenFriends, listenFriendRequests } from "./friends.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -17,11 +18,16 @@ document.addEventListener("DOMContentLoaded", () => {
     setupPostCreation(user);
     setupCommentCreation(user);
     loadPosts(user);
+    // Bạn có thể gọi listenFriends để cập nhật danh sách bạn bè của người dùng
+    // Ví dụ:
+    // listenFriends(friendsList => { /* cập nhật giao diện danh sách bạn bè */ });
+    // listenFriendRequests(requests => { /* cập nhật thông báo yêu cầu kết bạn */ });
   });
 
   setupChat();
   setupNotifications();
 
+  // Lắng nghe sự kiện mở phần bình luận khi nhấp vào bài viết
   document.addEventListener("openComments", (e) => {
     import("./comments.js").then((module) => {
       module.openComments(e.detail);
