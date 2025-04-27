e12923e1e4923f7a5e6ee5bed3743753ffa0279f
DrasaClone <drasaclone@gmail.com>
2025-04-27 11:08:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ export const database = getDatabase(app);
 
 // Khởi tạo PubNub
 export const pubnub = new PubNub({
-  publishKey: "YOUR_PUBNUB_PUBLISH_KEY",
+  publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4",
   subscribeKey: "YOUR_PUBNUB_SUBSCRIBE_KEY",
   uuid: "user_" + Math.random().toString(36).substring(2)
 });
