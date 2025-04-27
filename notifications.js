6684301802e0f9704b0ba516c2db2db729563735
DrasaClone <drasaclone@gmail.com>
2025-04-27 21:16:45 +0700

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,26 +1,25 @@
 // notifications.js
-
 import { pubnub } from "./config.js";
-
 const notificationArea = document.getElementById("notification-area");
 
 export function setupNotifications() {
-  pubnub.subscribe({ channels: ["forum-posts", "forum-comments"] });
-
+  pubnub.subscribe({ channels: ["forum-posts", "forum-comments", "friendRequests", "forum-chat"] });
   pubnub.addListener({
-    message: function (event) {
+    message: event => {
       let msgText = "";
       if (event.message.type === "new-post") {
         msgText = "Bài viết mới được đăng!";
       } else if (event.message.type === "new-comment") {
         msgText = "Có bình luận mới!";
+      } else if (event.message.type === "friend-request") {
+        msgText = "Có yêu cầu kết bạn mới!";
       }
       if (msgText) showNotification(msgText);
-    },
+    }
   });
 }
 
 function showNotification(text) {
   notificationArea.textContent = text;
-  setTimeout(() => (notificationArea.textContent = ""), 3000);
+  setTimeout(() => notificationArea.textContent = "", 3000);
 }
