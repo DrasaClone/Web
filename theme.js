fe809ac8c9201da28952e7f3df1ff67246b087ab
DrasaClone <drasaclone@gmail.com>
2025-05-01 06:59:44 +0700

    Delete theme.js

--- theme.js
+++
@@ -1,7 +0,0 @@
-// theme.js
-export function setupThemeToggle() {
-  const btn = document.getElementById("theme-toggle-btn");
-  btn.addEventListener("click", () => {
-    document.body.classList.toggle("darkmode");
-  });
-}
