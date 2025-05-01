56669c9e3075555454a2b8e6639ca0b3b1caf859
DrasaClone <drasaclone@gmail.com>
2025-05-01 11:37:39 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// Firebase
+const FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+// PubNub
+const PUBNUB_KEYS = { publishKey: "YOUR_PUB", subscribeKey: "YOUR_SUB" };
+const CHANNEL = "global_chat";
+// Cloudinary
+const CLOUDINARY = { cloudName: "YOUR_CLOUD", uploadPreset: "YOUR_UPLOAD_PRESET" };
+// Jitsi
+const JITSI_DOMAIN = "meet.jit.si";
+const JITSI_ROOM = "YourRoomName";
