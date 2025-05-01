0e7575ae2f26b918f2f6b879c40b469746354c8f
DrasaClone <drasaclone@gmail.com>
2025-05-01 11:58:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const FIREBASE_CONFIG = {
   appId: "YOUR_APP_ID"
 };
 // PubNub
-const PUBNUB_KEYS = { publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4", subscribeKey: "YOUR_SUB" };
+const PUBNUB_KEYS = { publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4", subscribeKey: "sub-c-0269ec54-430f-41b1-8a33-4200f566fcbb" };
 const CHANNEL = "global_chat";
 // Cloudinary
 const CLOUDINARY = { cloudName: "dgbux4wzo", uploadPreset: "okeqfdx4" };
