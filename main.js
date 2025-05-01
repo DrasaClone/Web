9351c6dc46c36b74dcf36af978db3d6446bad145
DrasaClone <drasaclone@gmail.com>
2025-05-01 22:47:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ firebase.initializeApp(firebaseConfig);
 
 // PubNub configuration
 const pubnub = new PubNub({
-  publishKey: "YOUR_PUBNUB_PUBLISH_KEY",
+  publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4",
   subscribeKey: "YOUR_PUBNUB_SUBSCRIBE_KEY",
   uuid: "user-" + Math.floor(Math.random() * 10000)
 });
