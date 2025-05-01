86d3df02f69c232c6d60dd4be3ec76d076f267d1
DrasaClone <drasaclone@gmail.com>
2025-05-01 12:01:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 // Firebase
 const FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCeYwTT7E8bi7bccIrc20MTe5S4r0e0wUI",
+  authDomain: "webai-7642b.firebaseapp.com",
+  databaseURL: "https://webai-7642b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webai-7642b",
+  storageBucket: "webai-7642b.firebasestorage.app",
+  messagingSenderId: "967881370128",
+  appId: "1:967881370128:web:e5c4b06e4f70f55a68b895",
+  measurementId: "G-61XJ390Q30"
 };
 // PubNub
 const PUBNUB_KEYS = { publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4", subscribeKey: "sub-c-0269ec54-430f-41b1-8a33-4200f566fcbb" };
