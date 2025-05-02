44bcb22646ebbb9f76040dea15f60324fcb87139
DrasaClone <drasaclone@gmail.com>
2025-05-02 19:28:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,14 @@
 
 // Firebase configuration – thay thế các giá trị dưới đây bằng thông tin dự án của bạn
 var firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_PROJECT.firebaseapp.com",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  databaseURL: "YOUR_FIREBASE_DATABASE_URL",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID"
+  apiKey: "AIzaSyCeYwTT7E8bi7bccIrc20MTe5S4r0e0wUI",
+  authDomain: "webai-7642b.firebaseapp.com",
+  databaseURL: "https://webai-7642b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webai-7642b",
+  storageBucket: "webai-7642b.firebasestorage.app",
+  messagingSenderId: "967881370128",
+  appId: "1:967881370128:web:e5c4b06e4f70f55a68b895",
+  measurementId: "G-61XJ390Q30"
 };
 // Khởi tạo Firebase
 firebase.initializeApp(firebaseConfig);
