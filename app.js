c616e7ec9ecbcd37dba5affecccb7add8fedf446
DrasaClone <drasaclone@gmail.com>
2025-05-02 19:30:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ firebase.initializeApp(firebaseConfig);
 // Khởi tạo PubNub – thay khóa của bạn
 var pubnub = new PubNub({
   publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4",
-  subscribeKey: "YOUR_PUBNUB_SUBSCRIBE_KEY"
+  subscribeKey: "sub-c-0269ec54-430f-41b1-8a33-4200f566fcbb"
 });
 var chatChannel = "global_chat";
 
