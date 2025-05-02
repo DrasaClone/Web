07ab88bbd9c0bbf7a8ad8ae59a719814de946a4f
DrasaClone <drasaclone@gmail.com>
2025-05-02 14:37:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ firebase.initializeApp(firebaseConfig);
 // Thay thế bằng publishKey và subscribeKey của bạn
 pubnub = new PubNub({
   publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4",
-  subscribeKey: "YOUR_PUBNUB_SUBSCRIBE_KEY",
+  subscribeKey: "sub-c-0269ec54-430f-41b1-8a33-4200f566fcbb",
   uuid: "user-" + Math.floor(Math.random() * 10000)
 });
 pubnub.subscribe({channels: ['chat']});
