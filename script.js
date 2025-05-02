748e6fbbc8509c5ae558822c067494a51c30d183
DrasaClone <drasaclone@gmail.com>
2025-05-02 14:15:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
   /* Khởi tạo PubNub – thay thế các khóa tương ứng */
   window.pubnub = new PubNub({
       publishKey: "pub-c-9ad32978-37b1-4f15-bc57-bac1884507a4",
-      subscribeKey: "YOUR_PUBNUB_SUBSCRIBE_KEY",
+      subscribeKey: "sub-c-0269ec54-430f-41b1-8a33-4200f566fcbb",
       uuid: generateUUID()
   });
   
