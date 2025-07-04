60de69a8e75d3b2d978ad26b1220a91c7581059f
DrasaClone <drasaclone@gmail.com>
2025-07-05 06:08:10 +0700

    Update index.js

--- android-webapp/www/js/index.js
+++ android-webapp/www/js/index.js
@@ -1,11 +1,13 @@
 // Cấu hình Firebase (thay YOUR_API_KEY, ...)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "${PUSH_SENDER_ID}",
-  appId: "YOUR_APP_ID"
+      apiKey: "AIzaSyCeYwTT7E8bi7bccIrc20MTe5S4r0e0wUI",
+      authDomain: "webai-7642b.firebaseapp.com",
+      databaseURL: "https://webai-7642b-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "webai-7642b",
+      storageBucket: "webai-7642b.firebasestorage.app",
+      messagingSenderId: "967881370128",
+      appId: "1:967881370128:web:e5c4b06e4f70f55a68b895",
+      measurementId: "G-61XJ390Q30"
 };
 
 if (window.cordova) {
