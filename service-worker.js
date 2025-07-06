366b0b1017bb4e1c29fb400cc027d3c650795954
DrasaClone <drasaclone@gmail.com>
2025-07-06 21:06:08 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,46 +2,57 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
+// Khi nâng cấp sw thì nâng v1.2 lên các số khác để cập nhật
+// service-worker.js
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const CACHE_NAME = 'ma-tran-giao-tiep-v1.2';
+// Các tệp cần thiết để ứng dụng chạy offline
+const urlsToCache = [
+  './',
+  './index.html',
+  './old.html'
+  // Khi bạn có các file css, js riêng, hãy thêm chúng vào đây
+  // './style.css',
+  // './main.js'
+];
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// 1. Cài đặt Service Worker và lưu cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache and added core files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting(); // Kích hoạt service worker mới ngay lập tức
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+// 2. Lấy dữ liệu từ cache khi offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Nếu có trong cache thì trả về, không thì fetch từ network
+        return response || fetch(event.request);
+      })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+// 3. Xóa các cache cũ khi có phiên bản mới
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
