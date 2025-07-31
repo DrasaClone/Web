1c7cd0db91a1049ebeb7057b1f220767fbdd18ff
DrasaClone <drasaclone@gmail.com>
2025-07-31 22:43:53 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Delete it when Google lập chỉ mục
+
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
@@ -47,4 +47,3 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
-Delete it */
