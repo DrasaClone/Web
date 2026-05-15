a6f5fac8551c5c2252f1d8475da206c3c04039db
DrasaClone <drasaclone@gmail.com>
2026-05-15 08:26:38 +0700

    Delete assets/js directory

--- assets/js/ui.js
+++
@@ -1,8 +0,0 @@
-export function initUI(templateId) {
-    const app = document.getElementById('app');
-    const template = document.getElementById(templateId);
-    if (template) {
-        const content = template.content.cloneNode(true);
-        app.appendChild(content);
-    }
-}
